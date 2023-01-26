01804d4f294788cad12225403b2ca7c152117499
onlyyasad <onlyyasad@gmail.com>
2023-01-26 18:53:32 +0600

    first commit

---
+++ intro.js
@@ -0,0 +1,20 @@
+var moneyHad = 1000;
+var orangePrice = 150.30;
+var applePrice = 100.50;
+var orangeQuantity = 2;
+var appleQuantity = 1;
+var totalPrice = (orangePrice * 1) + (applePrice * 2);
+var moneyRemaining = moneyHad - totalPrice;
+console.log(moneyRemaining)
+
+var mathematicsMark = 75.25;
+var biologyMark = 65;
+var chemistryMark = 80;
+var physicsMark = 35.45;
+var banglaMark = 99.50;
+var totalMark = mathematicsMark + biologyMark + chemistryMark + physicsMark + banglaMark;
+var totalSubject = 5;
+var averageMark = totalMark / totalSubject;
+averageMark = averageMark.toFixed(2)
+averageMark = parseFloat(averageMark)
+console.log(averageMark)
